86aab6236f1e32769909a4ad087cd48e1c983ff7
yashjugade <109807948+yashjugade@users.noreply.github.com>
2023-07-21 14:24:57 +0530

    Add files via upload

---
+++ Database.py
@@ -0,0 +1,31 @@
+import remoto
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "root"
+)
+
+
+cursor = mydb.cursor()
+
+cursor.execute("USE pennywise")
+
+def create_db():
+
+
+    cursor.execute("CREATE TABLE IF NOT EXISTS userinfo(userid INT PRIMARY KEY, firstname VARCHAR(255), lastname VARCHAR(255), dob DATE, age INT, city VARCHAR(100), noofdependents INT, mobile BIGINT, email VARCHAR(100))")
+    cursor.execute("CREATE TABLE IF NOT EXISTS transaction(transactionid INT , type VARCHAR(100), category VARCHAR(100), amount float, transdate DATE, transtime TIME)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS budget(amount float, category VARCHAR(100), note VARCHAR(300))")
+
+    cursor.execute("SHOW TABLES")
+
+def enter_data_budget(tamount, tcategory, tnote):
+    para=(tamount, tcategory, tnote)
+    stat=("INSERT INTO budget (amount, category, note) VALUES (%s %s %s)")
+    cursor.execute(stat, para)
+    #print(stat, para)
+
+
+enter_data_budget(1200, "transport", "HEllo")   
