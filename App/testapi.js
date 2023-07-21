1f05eafc922880ffb1ac897e5a1d78de7ed49336
Bhaavesh Waykole <bhaaveshw@gmail.com>
2023-07-21 17:43:40 +0530

    api-test-1

--- App/testapi.js
+++ App/testapi.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 import axios from 'axios';
 
-const App = () => {
+const Api = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -31,4 +31,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Api;
