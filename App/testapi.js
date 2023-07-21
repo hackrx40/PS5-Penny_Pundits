705116c9c912ebfebde339732207c4b2238fde0d
VK <56174314+vk-56@users.noreply.github.com>
2023-07-21 17:39:47 +0530

    Base Flask integration

---
+++ App/testapi.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Function to fetch data from the Python backend
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:5000/get_data');
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <View>
+      {data.map(item => (
+        <View key={item.id}>
+          <Text>Name: {item.name}</Text>
+          <Text>Price: {item.price}</Text>
+        </View>
+      ))}
+    </View>
+  );
+};
+
+export default App;
