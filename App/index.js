631be64294b5030dec4e4879f43099ea621b437d
Vishant Kalwani <vishanto95@gmail.com>
2023-07-21 10:43:25 +0530

    Our first commit - setting up styles

---
+++ App/index.js
@@ -0,0 +1,44 @@
+// import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View,ScrollView,SafeAreaView } from 'react-native';
+import {Stack , useRouter} from 'expo-router';
+
+import {COLORS , SIZES, icons ,images } from '../constants';
+import {Nearbyjobs, Popularjobs ,ScreenHeaderBtn,Welcome} from '../components';
+
+const Home = () => {
+    const router =useRouter();
+    return(
+
+    <SafeAreaView style={{ flex: 1,backgroundColor: COLORS.lightWhite}}>
+        <Stack.Screen 
+        options={{
+            headerStyle: {backgroundColor: COLORS.lightWhite},
+            headerShadowVisible: false,
+            headerLeft: () =>(
+                <ScreenHeaderBtn iconUrl={icons.plus} dimension="60%"/>
+            ),
+            headerRight: () =>(
+                <ScreenHeaderBtn iconUrl={icons.menu} dimension="100%"/>
+            ),
+            headerTitle: ""
+
+        }}/>
+        <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{
+            flex: 1,
+            padding: SIZES.medium
+        }}>
+            <Welcome />
+            <Popularjobs />
+            <Nearbyjobs />
+        </View>
+        </ScrollView>
+    </SafeAreaView>
+    )
+}
+
+export default Home;
+
+// Test push from Tejas
+// Test push from tanaya
---
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { TouchableOpacity , Image } from 'react-native'
+
+import styles from './screenheader.style'
+
+const ScreenHeaderBtn = ({ iconUrl , dimension , handlePress }) => {
+  return (
+    <View>
+      <TouchableOpacity style={styles.btnContainer}>
+        <Image
+        source={iconUrl}
+        resizeMode="cover"
+        style={styles.btnImg(dimension)}
+        />
+
+        
+      </TouchableOpacity>
+      
+    </View>
+  )
+}
+
+export default ScreenHeaderBtn
