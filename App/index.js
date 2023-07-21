6bf6dcb5c3310a6f22b97958d3bc1d74f8c46e4d
TejasAU <91965903+TejasAU@users.noreply.github.com>
2023-07-22 03:44:46 +0530

    Changed the button

--- App/index.js
+++ App/index.js
@@ -11,26 +11,9 @@ import Rewards from './Rewards';
 import {COLORS , SIZES, icons ,images } from '../constants';
 import {Nearbyjobs, Popularjobs ,ScreenHeaderBtn,Welcome} from '../components';
 import NumberPad from './numPad';
-import {Menu , MenuOptions , MenuOption , MenuTrigger , MenuProvider } from 'react-native-popup-menu'
 
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingTop: 50,
-      backgroundColor: '#ecf0f1',
-    },
-    button:{
-    backgroundColor: 'lightblue',
-    padding: 15,
-    borderRadius: 5,
-    textalign: 'center',
-    display: 'flex',
-    fontsize: 16,
-    margin: 4,
-    }
-  });
+
+
 
 
 const Home = () => {
@@ -55,25 +38,7 @@ const Home = () => {
         <ScrollView showsVerticalScrollIndicator={false}>
         
         <Welcome /> 
-        <MenuProvider style={styles.container}>
-        <View>
-          <Menu>
-            <MenuTrigger style={styles.button}
-            text="Open menu" />
-            <MenuOptions>
-              <MenuOption onSelect={() => alert(` You clicked on Save`)} text="Save" />
-              <MenuOption onSelect={() => alert(`You Clicked on Delete`)}>
-                <Text style={{ color: 'red' }}>Delete</Text>
-              </MenuOption>
-              <MenuOption
-                onSelect={() => alert(`Not called`)}
-                disabled={true}
-                text="Disabled"
-              />
-            </MenuOptions>
-          </Menu>
-        </View>
-      </MenuProvider>
+        
         
         <Budget />
         <NumberPad />
